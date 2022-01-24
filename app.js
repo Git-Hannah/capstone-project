8c61f50edcda84f6861cad458de7f3f9c96a1d6e
HannaHannah <hannahsenft@gmx.net>
2022-01-24 12:38:26 +0100

    add feature/create-files and prettify json

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
+import { promises as fs } from "fs";
+import { writeFile } from "fs/promises";
+
+
 console.log("Hello World");
 
+writeFile("fox", "fire");
+writeFile("hello.json", JSON.stringify({x:5, y:6}, null, 4));
+// 4 ist der replacer
